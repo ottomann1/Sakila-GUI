e8c9983f165f707718048d71f14c83d1caa5129b
ottomann1 <otto.kostmann@hotmail.com>
2022-03-30 13:42:42 +0200

    fixat och trixat

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,19 @@
 module com.example.sakilagui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.persistence;
+    requires java.sql;
+    requires transitive org.hibernate.orm.core;
+    requires javafx.base;
+    requires java.naming;
+    requires java.desktop;
+    requires transitive javafx.graphics;
+    requires java.base;
 
 
-    opens com.example.sakilagui to javafx.fxml;
+    opens com.example.sakilagui to javafx.fxml, org.hibernate.orm.core, javafx.base, java.base, Business, DAO, Database;
+    opens Business to javafx.fxml, org.hibernate.orm.core, javafx.base, java.base, com.example.sakilagui;
+    opens DAO to javafx.fxml, org.hibernate.orm.core, javafx.base, java.base, com.example.sakilagui;
+    opens Database to javafx.fxml, org.hibernate.orm.core, javafx.base, java.base, com.example.sakilagui;
     exports com.example.sakilagui;
 }

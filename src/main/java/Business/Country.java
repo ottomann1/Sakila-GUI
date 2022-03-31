ad41363e2c02dbb31a928690da1af26ba9af1b6a
Jens Radtke <jens.radtke@yaelev.se>
2022-03-31 21:30:49 +0200

    Country, Language

---
+++ src/main/java/Business/Country.java
@@ -0,0 +1,65 @@
+package Business;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+public class Country {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "country_id")
+    private int countryId;
+    @Basic
+    @Column(name = "country")
+    private String country;
+    @Basic
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    public Country(int countryId, String country, Timestamp lastUpdate) {
+        this.countryId = countryId;
+        this.country = country;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Country() {
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country1 = (Country) o;
+        return countryId == country1.countryId && Objects.equals(country, country1.country) && Objects.equals(lastUpdate, country1.lastUpdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(countryId, country, lastUpdate);
+    }
+}

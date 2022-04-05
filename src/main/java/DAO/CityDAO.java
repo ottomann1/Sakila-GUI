6090f8757e6005cc6cffc8dfdb585cc9acb0bfe5
ottomann1 <otto.kostmann@hotmail.com>
2022-04-05 13:54:23 +0200

    ändrade id i alla DAO klasser till short

---
+++ src/main/java/DAO/CityDAO.java
@@ -0,0 +1,61 @@
+package DAO;
+
+import Business.City;
+import Business.Country;
+import Database.Data;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CityDAO implements DAO {
+    @Override
+    public Optional read(long id) throws IOException, ClassNotFoundException {
+        Data data = new Data();
+        Optional<City> city = Optional.ofNullable((City) data.getData(City.class, (short) id));
+        return city;
+    }
+
+    @Override
+    public List readAll() throws IOException, ClassNotFoundException{
+        Data data = new Data();
+        List<Object[]> cityObjects = data.getDataListQuery("SELECT * FROM city");
+        List<City> citys = new ArrayList<City>();
+        for (Object[] o : cityObjects) {
+            City city = new City((short) o[0], o[1].toString(), (short) o[2], (Timestamp) o[3]);
+            citys.add(city);
+        }
+        return citys;
+    }
+
+
+    @Override
+    public void create(Object o) throws IOException, ClassNotFoundException {
+        Data data = new Data();
+        data.setData(o);
+
+    }
+
+    @Override
+    public void update(Object newT, Object oldT) throws IOException {
+        Data data = new Data();
+        data.updateData(newT);
+
+    }
+
+    @Override
+    public void delete(Object o) throws IOException {
+        Data data = new Data();
+        City city = (City) o;
+        data.deleteEm(City.class, city.getCityId());
+
+
+    }
+
+    @Override
+    public void deleteAll() throws IOException {
+
+    }
+}

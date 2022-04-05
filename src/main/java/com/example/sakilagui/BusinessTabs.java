d14fc19c1fee84e9147dcfc186234781c242d3e3
ottomann1 <otto.kostmann@hotmail.com>
2022-04-05 10:41:25 +0200

    jobbar på businessTabs

--- src/main/java/com/example/sakilagui/BusinessTabs.java
+++ src/main/java/com/example/sakilagui/BusinessTabs.java
@@ -1,21 +1,47 @@
 package com.example.sakilagui;
 
-import Business.Actor;
-import Business.Address;
-import Business.Customer;
+import Business.*;
+import DAO.FilmDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class BusinessTabs {
+
+    @FXML
+    private TableColumn<Actor, ?> actorFirstName;
+
+    @FXML
+    private TableColumn<Actor, ?> actorLastName;
 
     @FXML
     private Tab actorTab;
 
     @FXML
     private TableView<Actor> actorTable;
+
+    @FXML
+    private TableColumn<Address, ?> addressAddress;
+
+    @FXML
+    private TableColumn<Address, ?> addressCity;
+
+    @FXML
+    private TableColumn<Address, ?> addressPhone;
+
+    @FXML
+    private TableColumn<Address, ?> addressPostal;
 
     @FXML
     private Tab addressTab;
@@ -27,28 +53,108 @@ public class BusinessTabs {
     private AnchorPane ap;
 
     @FXML
+    private TableColumn<Customer, ?> customerAddress;
+
+    @FXML
+    private TableColumn<Customer, ?> customerEmail;
+
+    @FXML
+    private TableColumn<Customer, ?> customerFirstName;
+
+    @FXML
+    private TableColumn<Customer, ?> customerLastName;
+
+    @FXML
     private Tab customerTab;
 
     @FXML
     private TableView<Customer> customerTable;
 
     @FXML
+    private TableColumn<Film, ?> filmDescription;
+
+    @FXML
+    private TableColumn<Film, ?> filmLanguage;
+
+    @FXML
+    private TableColumn<Film, ?> filmLength;
+
+    @FXML
+    private TableColumn<Film, ?> filmReleaseYear;
+
+    @FXML
     private Tab filmTab;
 
     @FXML
-    private TableView<?> filmTable;
+    private TableView<Film> filmTable;
+
+    @FXML
+    private TableColumn<Film, ?> filmTitle;
+
+    @FXML
+    private TableColumn<Staff, ?> staffAddress;
+
+    @FXML
+    private TableColumn<Staff, ?> staffEmail;
+
+    @FXML
+    private TableColumn<Staff, ?> staffFirstName;
+
+    @FXML
+    private TableColumn<Staff, ?> staffLastName;
+
+    @FXML
+    private TableColumn<Staff, ?> staffStoreId;
 
     @FXML
     private Tab staffTab;
 
     @FXML
-    private TableView<?> staffTable;
+    private TableView<Staff> staffTable;
+
+    @FXML
+    private TableColumn<Store, ?> storeAddress;
+
+    @FXML
+    private TableColumn<Store, ?> storeStaff;
 
     @FXML
     private Tab storeTab;
 
     @FXML
-    private TableView<?> storeTable;
+    private TableView<Store> storeTable;
+
+    void filmLoad() throws IOException, ClassNotFoundException {
+        FilmDAO filmDAO = new FilmDAO();
+        List<Film> film = filmDAO.readAll();
+        ObservableList<Film> observableFilms = FXCollections.observableArrayList(film);
+        filmTable.setItems(observableFilms);
+        filmTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        filmDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        filmReleaseYear.setCellValueFactory(new PropertyValueFactory<>("releaseYear"));
+        filmLanguage.setCellValueFactory(new PropertyValueFactory<>("languageId"));
+        filmLength.setCellValueFactory(new PropertyValueFactory<>("length"));
+        filmTable.setItems(observableFilms);
+        filmTable.getColumns().setAll(filmTitle, filmDescription, filmReleaseYear, filmLanguage, filmLength);
+    }
+
+    void actorLoad(){
+
+    }
+
+    void storeLoad(){
+
+    }
+
+    void staffLoad(){
+
+    }
+    void customerLoad(){
+
+    }
+    void addressLoad(){
+
+    }
 
     @FXML
     void actorTabClick(ActionEvent event) {

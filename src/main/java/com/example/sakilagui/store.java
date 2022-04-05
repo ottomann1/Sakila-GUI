4639a689ba11c379b8423f6d090c1925d75c2966
lisahagberg <lisahagberg92@gmail.com>
2022-04-05 13:17:27 +0200

    Store fxml & controller added

---
+++ src/main/java/com/example/sakilagui/store.java
@@ -0,0 +1,32 @@
+package com.example.sakilagui;
+
+import Business.Address;
+import Business.Staff;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+public class store {
+
+    @FXML
+    private ComboBox<Address> addressDropDown;
+
+    @FXML
+    private ComboBox<Staff> staffDropDown;
+
+    @FXML
+    private TextArea staffListField;
+
+    @FXML
+    void addStaffOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addStoreOnClick(ActionEvent event) {
+
+    }
+
+}
+
